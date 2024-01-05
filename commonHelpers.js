94a09f0e79a755dbfceb14499c3626a77088007f
TetiZ <140248456+TetiZ@users.noreply.github.com>
2024-01-05 14:30:50 +0000

    Deploying to gh-pages from @ TetiZ/goit-js-hw-11@3c14fad9c581671200ad69c2787d536638b7f234 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,18 +1,35 @@
-import{i as m,S as d}from"./assets/vendor-46aac873.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&r(n)}).observe(document,{childList:!0,subtree:!0});function a(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=a(e);fetch(e.href,t)}})();const c=document.querySelector(".search-form");document.querySelector(".search-form-btn");const f=document.querySelector(".search-input"),i=document.querySelector(".gallery"),u=document.querySelector(".loader"),p=()=>u.dataset.loading="true",l=()=>u.dataset.loading="false",g=()=>{const o=f.value,a=`https://pixabay.com/api/?${new URLSearchParams({key:"41640115-31701c1b62cbb4a16b6499d34",q:`${o}`,image_type:"photo",orientation:"horizontal",safesearch:!0})}`;p(),fetch(a).then(r=>{if(!r.ok)throw new Error(r.status);return r.json()}).then(({hits:r})=>{l(),r.length===0?m.error({message:"Sorry, there are no images matching your search query. Please try again!"}):i.innerHTML=r.map(e=>`
+import{i as d,S as f}from"./assets/vendor-46aac873.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&r(i)}).observe(document,{childList:!0,subtree:!0});function a(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=a(e);fetch(e.href,t)}})();const c=document.querySelector(".search-form");document.querySelector(".search-form-btn");const p=document.querySelector(".search-input"),n=document.querySelector(".gallery"),m=document.querySelector(".loader"),u=()=>m.classList.add("active"),l=()=>m.classList.remove("active"),g=()=>{const o=p.value,a=`https://pixabay.com/api/?${new URLSearchParams({key:"41640115-31701c1b62cbb4a16b6499d34",q:`${o}`,image_type:"photo",orientation:"horizontal",safesearch:!0})}`;u(),fetch(a).then(r=>{if(!r.ok)throw new Error(r.status);return r.json()}).then(({hits:r})=>{l(),r.length===0?d.error({message:"Sorry, there are no images matching your search query. Please try again!"}):n.innerHTML=r.map(e=>`
               
+              <li class="gallery-item">
                 <a class="gallery-link" href=${e.largeImageURL}>
-                  <img
-                    class="gallery-image"
-                    src=${e.webformatURL}
-                    alt=${e.tags}
-                  />
-                
-                <div class="img-info-wrapper">
-                  <p class="img-info">Likes: ${e.likes}</p>
-                  <p class="img-info">Views: ${e.views}</p>
-                  <p class="img-info">Comments: ${e.comments}</p>
-                  <p class="img-info">Downloads: ${e.downloads}</p>
-                </div>
+                    <img
+                      class="gallery-image"
+                      src=${e.webformatURL}
+                      alt=${e.tags}
+                    />
+                  
+                  <div class="img-info-wrapper">
+                    <div class="img-part-info-wrapper">
+                    <h3 class="img-info-title">Likes</h3>
+                      <p class="img-info">${e.likes}</p>
+                    </div>
+  
+                    <div class="img-part-info-wrapper">
+                    <h3 class="img-info-title">Views</h3>
+                      <p class="img-info">${e.views}</p>
+                    </div>
+  
+                    <div class="img-part-info-wrapper">
+                    <h3 class="img-info-title">Comments</h3>
+                      <p class="img-info">${e.comments}</p>
+                    </div>
+  
+                    <div class="img-part-info-wrapper">
+                    <h3 class="img-info-title">Downloads</h3>
+                      <p class="img-info">${e.downloads}</p>
+                    </div>
+                  </div>
               </a>
-            `).join(""),new d(".gallery a",{captionDelay:250,captionsData:"alt"}),i.refresh()}).catch(r=>{console.log(r),l()})},y=o=>{o.preventDefault(),g(),c.reset()};c.addEventListener("submit",y);
+            </li>
+            `).join(""),new f(".gallery a",{captionDelay:250,captionType:"attr",captionsData:"alt"}),n.refresh()}).catch(r=>{console.log(r),l()})},h=o=>{o.preventDefault(),g(),c.reset()};c.addEventListener("submit",h);
 //# sourceMappingURL=commonHelpers.js.map
